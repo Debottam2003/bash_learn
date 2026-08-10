cb299c55e6485fd452c501ce58de4fb311397a60
Debottam Kar <debottamkar2003@gmail.com>
2026-08-10 14:03:02 +0530

    switch-case

---
+++ advance_shell/switch_case.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+s=$1
+ 
+case "$s" in
+    debottam)
+        echo "hi debottam"    
+        ;;
+    rony)
+        echo "ohh there he is"
+        ;;
+    guy)
+       echo "uh oh here comes trouble"
+       ;;
+    *)
+       echo "just do anything"
+       ;;
+esac
+
+# if [[ ${S} == "debottam" ]];
+# then
+#     echo "hi debottam" 
+# elif [[ ${S} == "rony" ]];
+# then
+#     echo "ohh there he is"
+# else 
+#     echo "uh oh here comes trouble"
+# fi
