6ad98015feb64c98cc2d5c4ae402937698dd2f00
Debottam Kar <debottamkar2003@gmail.com>
2026-08-27 20:39:23 +0530

    array

--- advance_shell/arr.sh
+++ advance_shell/arr.sh
@@ -3,8 +3,32 @@ arr=(debottam rony okudera ichinose)
 echo ${arr[@]} # Prints the entire array
 echo ${!arr[@]} # Prints the indices of the array
 echo ${#arr[@]} # Prints the lenght of the array
+echo ${arr[*]}
+
+echo "================================"
 
 for i in ${!arr[@]};
 do
 echo ${arr[$i]}
 done
+
+echo "================================"
+
+for item in ${arr[@]};
+do
+echo $item
+done
+
+echo "================================"
+
+echo "${arr[0]}"
+echo "${arr[1]}"
+echo "${arr[2]}"
+echo "${arr[3]}"
+
+echo "================================"
+
+echo "${arr[-1]}"
+echo "${arr[-2]}"
+echo "${arr[-3]}"
+echo "${arr[-4]}"
